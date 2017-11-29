cc0abee3d350ee4dc2bd67d9fe3fb947f91133e0
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2017-11-29 19:43:49 -0200

    `evalType` no `BooleanValue`

--- src/main/scala/com/minihaskell/BooleanValue.scala
+++ src/main/scala/com/minihaskell/BooleanValue.scala
@@ -1,4 +1,5 @@
 package com.minihaskell
 
-case class BooleanValue(val value: Boolean)
-  extends ConcreteValue[Boolean](value)
+case class BooleanValue(val value: Boolean) extends ConcreteValue[Boolean](value) {
+  override def evalType(): Type = BooleanType()
+}
