f3a51b9b06b0d559c2f915c684a290c7f051dd1f
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2017-11-29 19:46:05 -0200

    inserindo tipo da expressao let no construtor

--- src/main/scala/com/minihaskell/LetExpression.scala
+++ src/main/scala/com/minihaskell/LetExpression.scala
@@ -1,10 +1,13 @@
 package com.minihaskell
 
 import com.minihaskell.memory.RunningEnvironment
+import com.minihaskell.memory.Gama
 
 class LetExpression( val id : String, 
                      val namedExpression : Expression, 
                      val body : Expression ) extends Expression {
+  
+  Gama.insert( id, namedExpression.evalType() )
   
   override def eval(): Value = {
     val value = namedExpression.eval()
