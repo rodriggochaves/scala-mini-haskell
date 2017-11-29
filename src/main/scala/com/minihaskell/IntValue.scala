e16775119c3dd48b3e601ba0486c08378b96b078
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2017-11-29 19:44:30 -0200

    `evalType` em `IntValue`

--- src/main/scala/com/minihaskell/IntValue.scala
+++ src/main/scala/com/minihaskell/IntValue.scala
@@ -1,3 +1,5 @@
 package com.minihaskell
 
-case class IntValue( val value: Integer ) extends ConcreteValue[Integer](value)
+case class IntValue( val value: Integer ) extends ConcreteValue[Integer](value) {
+  override def evalType(): Type = IntegerType()
+}
