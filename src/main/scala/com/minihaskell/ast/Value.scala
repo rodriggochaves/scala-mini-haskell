ae48604f409fc79b9ccb3aea92bca16b8d48b43c
Kevin Souto <soutokevin@gmail.com>
2017-12-01 10:33:59 -0200

    adiciona função na ast

--- src/main/scala/com/minihaskell/ast/Value.scala
+++ src/main/scala/com/minihaskell/ast/Value.scala
@@ -1,5 +1,7 @@
 package com.minihaskell.ast
 
+import com.minihaskell.memory.RunningEnvironment
+
 sealed trait Value extends Expression
 
 sealed abstract class IrreducibleValue extends Value {
@@ -10,3 +12,12 @@ case class BooleanValue(value: Boolean) extends IrreducibleValue
 case class IntValue(value: Integer)     extends IrreducibleValue
 case class Closure(id: String, body: Expression, env: Map[String, Value])
   extends IrreducibleValue
+
+class Function(name: String, args: Map[String, Unit], body: Expression)
+  extends IrreducibleValue {
+
+  override def eval() = {
+    RunningEnvironment.update(name, this)
+    this
+  }
+}
