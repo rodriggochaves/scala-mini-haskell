1ac7ea641bd89c7143df5acbeb53ffba648e579f
Kevin Souto <soutokevin@gmail.com>
2017-11-30 22:44:01 -0200

    faz com que testes limpem o ambiente antes de rodarem

--- src/test/scala/com/minihaskell/LambdaApplicationSpec.scala
+++ src/test/scala/com/minihaskell/LambdaApplicationSpec.scala
@@ -1,10 +1,12 @@
 package com.minihaskell.ast
 
 import org.scalatest._
+import com.minihaskell.memory.RunningEnvironment
 
 class LambdaApplicationSpec extends FlatSpec with Matchers {
 
   it should "`((x) -> x + 1) , 5` == 6" in {
+    RunningEnvironment.clean()
     val inc = new LambdaExpression("x", new SumExpression(new ReferenceExpression("x"), IntValue(1)))
     val app = new LambdaApplication(inc, IntValue(5))
 
@@ -12,6 +14,7 @@ class LambdaApplicationSpec extends FlatSpec with Matchers {
   }
 
   it should "`( let y = 10 in let f = (x -> x + y) in let y = 20 in f(10) )` == 20" in {
+    RunningEnvironment.clean()
     val let1 = new LetExpression("y", IntValue(20), LambdaApplication(
                                                       ReferenceExpression("f"), IntValue(10)))
     val let2 = new LetExpression("f", new LambdaExpression("x",
--- src/test/scala/com/minihaskell/exceptions/UndeclaredVariableExpectionSpec.scala
+++ src/test/scala/com/minihaskell/exceptions/UndeclaredVariableExpectionSpec.scala
@@ -2,10 +2,12 @@ package com.minihaskell.exceptions
 
 import org.scalatest._
 import com.minihaskell.ast._
+import com.minihaskell.memory.RunningEnvironment
 
 class LetExpSpec extends FlatSpec with Matchers {
 
   it should "( let x = 10 in x + y ) raise Exception" in {
+    RunningEnvironment.clean()
     var let = new LetExpression("x", IntValue(10),
       new SumExpression(new ReferenceExpression("x"), new ReferenceExpression("y")))
 
