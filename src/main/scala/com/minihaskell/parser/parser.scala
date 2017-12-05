1e64e1e3f4761ea555b9863362b7f92e99afe32f
Kevin Souto <soutokevin@gmail.com>
2017-12-05 21:46:00 -0200

    parser com suporte as operações aritméticas básicas

---
+++ src/main/scala/com/minihaskell/parser/parser.scala
@@ -0,0 +1,75 @@
+package com.minihaskell.parser
+
+import scala.util.parsing.combinator._
+import scala.util.parsing.input.{NoPosition, Position, Reader}
+
+import com.minihaskell.ast._
+
+object Parser extends Parsers {
+  override type Elem = Token
+
+  class TokenReader(tokens: List[Token])
+    extends Reader[Token] {
+
+    override def first: Token = tokens.head
+    override def atEnd: Boolean = tokens.isEmpty
+    override def pos: Position = NoPosition
+    override def rest: Reader[Token] = new TokenReader(tokens.tail)
+  }
+
+  def apply(str: String): Either[(Int, Int), Expression] = {
+    Lexer(str) match {
+      case Right(tokens) => apply(tokens)
+      case Left(pos) => Left(pos)
+    }
+  }
+
+  def apply(tokens: List[Token]): Either[(Int, Int), Expression] = {
+    val reader = new TokenReader(tokens)
+
+    expression(reader) match {
+      case NoSuccess(msg, next) => Left((next.pos.line, next.pos.column))
+      case Success(result, next) => Right(result)
+    }
+  }
+
+  def expression: Parser[Expression] = {
+    add | sub | mult | div | number | identifier
+  }
+
+  def add: Parser[AddExpression] = {
+    (operand ~ ADD ~ operand) ^^ {
+      case lhs ~ _ ~ rhs => AddExpression(lhs, rhs)
+    }
+  }
+
+  def sub: Parser[SubExpression] = {
+    (operand ~ SUB ~ operand) ^^ {
+      case lhs ~ _ ~ rhs => SubExpression(lhs, rhs)
+    }
+  }
+
+  def mult: Parser[MultiplicationExpression] = {
+    (operand ~ MULT ~ operand) ^^ {
+      case lhs ~ _ ~ rhs => MultiplicationExpression(lhs, rhs)
+    }
+  }
+
+  def div: Parser[DivisionExpression] = {
+    (operand ~ DIV ~ operand) ^^ {
+      case lhs ~ _ ~ rhs => DivisionExpression(lhs, rhs)
+    }
+  }
+
+  def operand: Parser[Expression] = {
+    number | identifier
+  }
+
+  private def identifier: Parser[ReferenceExpression] = {
+    accept("identifier", { case IDENTIFIER(name) => ReferenceExpression(name) })
+  }
+
+  private def number: Parser[IntValue] = {
+    accept("number", { case NUMBER(value) => IntValue(value.toInt) })
+  }
+}
