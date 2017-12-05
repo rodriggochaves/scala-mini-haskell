e6661c66c20eeef5acc51090abc472c7e9cec70d
Kevin Souto <soutokevin@gmail.com>
2017-12-05 21:13:30 -0200

    implementa um lexer

---
+++ src/main/scala/com/minihaskell/parser/lexer.scala
@@ -0,0 +1,63 @@
+package com.minihaskell.parser
+
+import scala.util.parsing.combinator._
+
+object Lexer extends RegexParsers {
+  override def skipWhitespace = true
+  override val whiteSpace = "[ \t\r\f]+".r
+
+  def apply(code: String): Either[(Int, Int), List[Token]] = {
+    parse(tokens, code) match {
+      case NoSuccess(msg, next)  => Left((next.pos.line, next.pos.column))
+      case Success(result, next) => Right(result)
+    }
+  }
+
+  def tokens: Parser[List[Token]] = {
+    phrase(rep1(
+        let
+      | in
+      | _if
+      | then
+      | _else
+      | arrow
+      | or
+      | equal
+      | and
+      | not
+      | div
+      | mult
+      | sub
+      | add
+      | number
+      | identifier
+      | open_paren
+      | close_paren
+    ))
+  }
+
+  def identifier: Parser[IDENTIFIER] = {
+    "[a-zA-Z_][a-zA-Z0-9_]*".r ^^ { str => IDENTIFIER(str) }
+  }
+
+  def number: Parser[NUMBER] = {
+    "[0-9]+".r ^^ { str => NUMBER(str) }
+  }
+
+  def let           = "let"           ^^ { _ => LET }
+  def in            = "in"            ^^ { _ => IN }
+  def _if           = "if"            ^^ { _ => IF }
+  def then          = "then"          ^^ { _ => THEN }
+  def _else         = "else"          ^^ { _ => ELSE }
+  def arrow         = "->"            ^^ { _ => ARROW }
+  def or            = "||"            ^^ { _ => OR }
+  def equal         = "="             ^^ { _ => EQUAL }
+  def and           = "&&"            ^^ { _ => AND }
+  def not           = "!"             ^^ { _ => NOT }
+  def div           = "/"             ^^ { _ => DIV }
+  def mult          = "*"             ^^ { _ => MULT }
+  def sub           = "-"             ^^ { _ => SUB }
+  def add           = "+"             ^^ { _ => ADD }
+  def open_paren    = "("             ^^ { _ => OPEN_PAREN }
+  def close_paren   = ")"             ^^ { _ => CLOSE_PAREN }
+}
---
+++ src/main/scala/com/minihaskell/parser/tokens.scala
@@ -0,0 +1,22 @@
+package com.minihaskell.parser
+
+sealed trait Token
+
+case class  NUMBER(str: String)      extends Token
+case class  IDENTIFIER(str: String)  extends Token
+case object IF                       extends Token
+case object THEN                     extends Token
+case object ELSE                     extends Token
+case object IN                       extends Token
+case object LET                      extends Token
+case object EQUAL                    extends Token
+case object OR                       extends Token
+case object AND                      extends Token
+case object NOT                      extends Token
+case object ARROW                    extends Token
+case object DIV                      extends Token
+case object MULT                     extends Token
+case object SUB                      extends Token
+case object ADD                      extends Token
+case object OPEN_PAREN               extends Token
+case object CLOSE_PAREN              extends Token
