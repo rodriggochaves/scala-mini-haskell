e0ea4f5b1e7951ee73f01f07b2495ddaef598b3f
Kevin Souto <soutokevin@gmail.com>
2017-12-06 14:34:32 -0200

    reformata código; adiciona teste

--- src/main/scala/com/minihaskell/ast/IfThenElseExpression.scala
+++ src/main/scala/com/minihaskell/ast/IfThenElseExpression.scala
@@ -1,18 +1,17 @@
 package com.minihaskell.ast
+
 import com.minihaskell.exceptions.InvalidExpressionException
 
-case class IfThenElseExpression( cond: Expression, then: Expression, _else: Expression ) 
-     extends Expression{
+case class IfThenElseExpression(cond: Expression,
+                                then: Expression,
+                                _else: Expression) extends Expression {
 
   override def eval(): Value = {
     cond.eval() match{
-      case BooleanValue(res) => {
-        if(res){
-          then.eval()
-        }
-        else{
-          _else.eval()
-        }
+      case BooleanValue(res) => if (res) {
+        then.eval()
+      } else {
+        _else.eval()
       }
       case _ => throw new InvalidExpressionException
     }
--- src/test/scala/com/minihaskell/IfThenElseSpec.scala
+++ src/test/scala/com/minihaskell/IfThenElseSpec.scala
@@ -3,13 +3,26 @@ package com.minihaskell.ast
 import org.scalatest._
 
 class IfThenElseSpec extends FlatSpec with Matchers {
-    
-  it should "be evaluated to Value(25) when true && true then 5 + 5 else 5 * 5" in {
-    val comp = new IfThenElseExpression(new AndExpression(BooleanValue(false), BooleanValue(true)), 
-                                    new SumExpression(IntValue(5), IntValue(5)),
-                                    new MultiplicationExpression(IntValue(5), IntValue(5)))
+
+  it should "be evaluated to IntValue(25) when if true && false then 5 + 5 else 5 * 5" in {
+    val comp = new IfThenElseExpression(
+      AndExpression(BooleanValue(true), BooleanValue(false)),
+      AddExpression(IntValue(5), IntValue(5)),
+      MultiplicationExpression(IntValue(5), IntValue(5))
+    )
     val res = comp.eval().asInstanceOf[IntValue]
 
     res should be (IntValue(25))
+  }
+
+  it should "be evaluated to IntValue(10) when if true || false then 5 + 5 else 5 * 5" in {
+    val comp = new IfThenElseExpression(
+      OrExpression(BooleanValue(true), BooleanValue(false)),
+      AddExpression(IntValue(5), IntValue(5)),
+      MultiplicationExpression(IntValue(5), IntValue(5))
+    )
+    val res = comp.eval().asInstanceOf[IntValue]
+
+    res should be (IntValue(10))
   }
 }
