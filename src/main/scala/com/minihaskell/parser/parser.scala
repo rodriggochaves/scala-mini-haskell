895d791fb3ee01df00e1f26cb5d457d7336ce057
Kevin Souto <soutokevin@gmail.com>
2017-12-06 18:21:52 -0200

    parse de chamada de função

--- src/main/scala/com/minihaskell/parser/parser.scala
+++ src/main/scala/com/minihaskell/parser/parser.scala
@@ -38,7 +38,7 @@ object Parser extends Parsers {
   }
 
   def expression: Parser[Expression] = {
-    ifThenElse | lambda | let | add | sub | mult | div | number | identifier
+    call | paren | ifThenElse | lambda | let | add | sub | mult | div | number | identifier
   }
 
   def add: Parser[AddExpression] = {
@@ -92,6 +92,12 @@ object Parser extends Parsers {
     (IF ~ expression ~ THEN ~ expression ~ ELSE ~ expression) ^^ {
       case _ ~ cond ~ _ ~ then ~ _ ~ _else  =>
         IfThenElseExpression(cond, then, _else)
+    }
+  }
+
+  def call: Parser[CallExpression] = {
+    (operand ~ operand ~ operand.*) ^^ {
+      case func ~ first ~ params => CallExpression(func, first::params)
     }
   }
 
