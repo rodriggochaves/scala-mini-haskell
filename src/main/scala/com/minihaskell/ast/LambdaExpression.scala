dee430f956330c824c95ae2b4abb42681f2024df
Kevin Souto <soutokevin@gmail.com>
2017-12-07 08:07:15 -0200

    tipagem para lambda expressions

--- src/main/scala/com/minihaskell/ast/LambdaExpression.scala
+++ src/main/scala/com/minihaskell/ast/LambdaExpression.scala
@@ -1,11 +1,14 @@
 package com.minihaskell.ast
 
-import com.minihaskell.memory.RunningEnvironment
+import com.minihaskell.memory.{Gama, RunningEnvironment}
 
 class LambdaExpression(val id: String, val t: Type, val body: Expression)
   extends Expression {
 
   override def eval(): Value =
-    Closure(id, ErrorType, body, RunningEnvironment.getCurrent)
-  override def evalType(): Type = body.evalType()
+    Closure(id, t, body, RunningEnvironment.getCurrent)
+  override def evalType(): Type = {
+    Gama.insert(id, t)
+    FnType(t :: Nil, body.evalType())
+  }
 }
--- src/main/scala/com/minihaskell/ast/Types.scala
+++ src/main/scala/com/minihaskell/ast/Types.scala
@@ -2,7 +2,7 @@ package com.minihaskell.ast
 
 sealed trait Type
 
-object     ErrorType                                extends Type
-object     IntegerType                              extends Type
-object     BooleanType                              extends Type
-case class FnType(from: List[Type], to: List[Type]) extends Type
+object     ErrorType                          extends Type
+object     IntegerType                        extends Type
+object     BooleanType                        extends Type
+case class FnType(from: List[Type], to: Type) extends Type
