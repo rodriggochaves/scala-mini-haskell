d0693f77f0ce012302712fb2b0d7a731b1eb87df
Fillype Nascimento <fillype.n@outlook.com>
2017-12-08 11:09:46 -0200

    Operadores relacionais

--- src/main/scala/com/minihaskell/ast/BasicExpressions.scala
+++ src/main/scala/com/minihaskell/ast/BasicExpressions.scala
@@ -147,3 +147,117 @@ case class NotExpression(exp: Expression) extends Expression {
     if (t1 == BooleanType) BooleanType else ErrorType
   }
 }
+
+// Relational Expressions ---------------------------------------------------------
+class GreaterExpression(val lhs: Expression, val rhs: Expression)
+  extends Expression {
+
+  override def eval(): Value = {
+    val v1 = lhs.eval().asInstanceOf[IntValue]
+    val v2 = rhs.eval().asInstanceOf[IntValue]
+
+    return BooleanValue(v1.value > v2.value)
+  }
+
+  override def evalType(): Type = {
+    val t1 = lhs.evalType()
+    val t2 = rhs.evalType()
+
+    if (t1 == IntegerType && t2 == IntegerType) {
+      BooleanType
+    } else {
+      ErrorType
+    }
+  }
+}
+
+class LessExpression(val lhs: Expression, val rhs: Expression)
+  extends Expression {
+
+  override def eval(): Value = {
+    val v1 = lhs.eval().asInstanceOf[IntValue]
+    val v2 = rhs.eval().asInstanceOf[IntValue]
+
+    return BooleanValue(v1.value < v2.value)
+  }
+
+  override def evalType(): Type = {
+    val t1 = lhs.evalType()
+    val t2 = rhs.evalType()
+
+    if (t1 == IntegerType && t2 == IntegerType) {
+      BooleanType
+    } else {
+      ErrorType
+    }
+  }
+}
+
+class GreaterOrEqualExpression(val lhs: Expression, val rhs: Expression)
+  extends Expression {
+
+  override def eval(): Value = {
+    val v1 = lhs.eval().asInstanceOf[IntValue]
+    val v2 = rhs.eval().asInstanceOf[IntValue]
+
+    return BooleanValue(v1.value >= v2.value)
+  }
+
+  override def evalType(): Type = {
+    val t1 = lhs.evalType()
+    val t2 = rhs.evalType()
+
+    if (t1 == IntegerType && t2 == IntegerType) {
+      BooleanType
+    } else {
+      ErrorType
+    }
+  }
+}
+
+class LessOrEqualExpression(val lhs: Expression, val rhs: Expression)
+  extends Expression {
+
+  override def eval(): Value = {
+    val v1 = lhs.eval().asInstanceOf[IntValue]
+    val v2 = rhs.eval().asInstanceOf[IntValue]
+
+    return BooleanValue(v1.value <= v2.value)
+  }
+
+  override def evalType(): Type = {
+    val t1 = lhs.evalType()
+    val t2 = rhs.evalType()
+
+    if (t1 == IntegerType && t2 == IntegerType) {
+      BooleanType
+    } else {
+      ErrorType
+    }
+  }
+}
+
+//class EqualExpression(val lhs: Expression, val rhs: Expression)
+//  extends Expression {
+//
+//  override def eval(): Value = {
+//    val v1 = lhs.eval().asInstanceOf[IntValue]
+//    val v2 = rhs.eval().asInstanceOf[IntValue]
+//
+//    return BooleanValue(v1 <= v2)
+//  }
+//
+//  override def evalType(): Type = {
+//    val v1 = lhs.evalType()
+//    val v2 = rhs.evalType()
+//
+//    if((v1 == ErrorType) || (v2 == ErrorType)){
+//      return ErrorType
+//    }
+//    else{
+//      if(v1 != v2){
+//        throw new InvalidExpressionException
+//      }
+//    }
+//  }
+//}
